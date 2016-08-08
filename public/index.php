2896ba2cba0c1ef15960bca42f25dc457bbd2eaf
Ryan Albrecht <ryan@ryanalbrecht.ca>
2016-08-07 17:11:58 -0700

    Initial commit
    Zoom tracks both sides of the map. Location tracks each map at the top of the page, and can be changed with submit event.

---
+++ public/index.php
@@ -0,0 +1,45 @@
+<?php
+$config = array(
+  'google_maps' => array(
+    'endpoint' => 'https://maps.googleapis.com/maps/api/js',
+    'query_args' => array(
+      'v' => '3.24',
+      'key' => 'AIzaSyB70zXqgMf5Ko3bRci8USuDoU5D_nkgQYc',
+      'callback' => 'initMap',
+    ),
+  ),
+);
+
+function getURLFromConfig($config) {
+  return
+    $config['endpoint'] .
+    '?' .
+    ($config['query_args'] ? http_build_query($config['query_args']) : '');
+}
+
+?><!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="/main.css">
+  </head>
+  <body>
+    <div class="serach__container">
+      <div class="search__left">
+        <form id="locationLeft">
+          <input id="inputLeft" type="text" />
+        </form>
+      </div>
+      <div class="search__right">
+        <form id="locationRight">
+          <input id="inputRight" type="text" />
+        </form>
+      </div>
+    </div>
+    <div class="map__container">
+      <div id="mapLeft" class="map__left"></div>
+      <div id="mapRight" class="map__right"></div>
+    </div>
+    <script src="/startup.js"></script>
+    <script src="<?= getURLFromConfig($config['google_maps']) ?>" async defer></script>
+  </body>
+</html>
---
+++ public/startup.js
@@ -0,0 +1,144 @@
+function throttle(fn, threshhold, scope) {
+  threshhold || (threshhold = 250);
+  var last,
+      deferTimer;
+  return function () {
+    var context = scope || this;
+
+    var now = +new Date,
+        args = arguments;
+    if (last && now < last + threshhold) {
+      // hold on to it
+      clearTimeout(deferTimer);
+      deferTimer = setTimeout(function () {
+        last = now;
+        fn.apply(context, args);
+      }, threshhold);
+    } else {
+      last = now;
+      fn.apply(context, args);
+    }
+  };
+}
+
+function sprintf(string, arg) {
+  return Array.prototype.slice.call(arguments, 1)
+    .reduce(function(str, arg) {
+      return str.replace('%s', arg);
+    }, string);
+}
+
+function fromID(id) {
+  if (!fromID.__cached) {
+    var ids = [
+      'mapLeft',
+      'mapRight',
+      'locationLeft',
+      'locationRight',
+      'inputLeft',
+      'inputRight',
+    ];
+    fromID.__cached = ids.reduce(function(cache, id) {
+      cache[id] = document.getElementById(id);
+      return cache;
+    }, {});
+  }
+  if (!fromID.__cached[id]) {
+    throw new Error(sprintf('Element with id %s not found', id));
+  }
+  return fromID.__cached[id];
+}
+
+function initMap() {
+  var model = new google.maps.MVCObject();
+  model.set('zoom', 8);
+
+  var mapLeft = getMap(fromID('mapLeft'));
+  var mapRight = getMap(fromID('mapRight'));
+
+  mapLeft.bindTo('zoom', model);
+  mapRight.bindTo('zoom', model);
+
+  var locationLeft = fromID('locationLeft');
+  var locationRight = fromID('locationRight');
+
+  var inputLeft = fromID('inputLeft');
+  var inputRight = fromID('inputRight');
+
+  locationLeft.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    codeAddress(geocoder, inputLeft.value, function(location) {
+      mapLeft.setCenter(location);
+    });
+  });
+  locationRight.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    codeAddress(geocoder, inputRight.value, function(location) {
+      mapRight.setCenter(location);
+    });
+  });
+
+  var geocoder = new google.maps.Geocoder;
+  trackMapLocation(geocoder, mapLeft, inputLeft);
+  trackMapLocation(geocoder, mapRight, inputRight);
+
+  window.model = model;
+  window.mapLeft = mapLeft;
+  window.mapRight = mapRight;
+}
+
+function getMap(elem) {
+  return new google.maps.Map(elem, {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: true,
+    zoom: 8
+  });
+}
+
+function trackMapLocation(geocoder, map, input) {
+  var renderResults = function(result) {
+    input.value = result;
+  };
+
+  geocodeLatLng(geocoder, {
+    lat: map.getCenter().lat(),
+    lng: map.getCenter().lng(),
+  }, renderResults);
+
+  map.addListener('center_changed', throttle(function() {
+    geocodeLatLng(geocoder, {
+      lat: map.getCenter().lat(),
+      lng: map.getCenter().lng(),
+    }, renderResults);
+  }));
+}
+
+function geocodeLatLng(geocoder, location, callback) {
+  geocoder.geocode({'location': location}, function(results, status) {
+    if (status === 'OK') {
+      if (results.length) {
+        callback(results[0].formatted_address);
+        return;
+      } else {
+        console.log('No results found for', location);
+      }
+    } else {
+      console.log('Geocoder failed due to: ' + status);
+    }
+
+    callback(location.lat + ', ' + location.lng);
+  });
+}
+
+function codeAddress(geocoder, address, callback) {
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status == 'OK') {
+        callback(results[0].geometry.location);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
